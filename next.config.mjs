8cc5bb27e852c9f82255fab7d0633725701674b5
ozankersit <ozankersit@gmail.com>
2024-10-17 14:37:15 +0300

    eslint setup and next config updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'rickandmortyapi.com',
-            }
-        ]
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rickandmortyapi.com",
+      },
+    ],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
     },
-    redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/characters/1',
-                permanent: true
-            }
-        ]
-    }
+  },
+  redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/characters/1",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
